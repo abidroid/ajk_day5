7b5b7321e1ef95aee22b252025458eb12196b5bc
Abid <abidroid1@gmail.com>
2025-05-23 11:43:01 +0500

    quotes api

--- lib/screens/api_demo_screen.dart
+++ lib/screens/api_demo_screen.dart
@@ -1,6 +1,7 @@
-
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ApiDemoScreen extends StatefulWidget {
   const ApiDemoScreen({super.key});
@@ -10,6 +11,33 @@ class ApiDemoScreen extends StatefulWidget {
 }
 
 class _ApiDemoScreenState extends State<ApiDemoScreen> {
+  // Initial State
+  String quote = 'Quote';
+
+  Future<void> getRandomQuote() async {
+    String url = "https://dummyjson.com/quotes/random";
+
+    // 1. send request to server
+    // 2. receive response
+    http.Response response = await http.get(Uri.parse(url));
+
+    // 3. check status code
+    if( response.statusCode == 200 ){
+      // 4. if status code is 200
+      // 5. means we got response
+
+      var jsonResponse = jsonDecode(response.body);
+
+      quote  = jsonResponse['quote'];
+      setState(() {
+
+      });
+    }
+
+
+    // 6. we need to extract quote from the response
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +45,18 @@ class _ApiDemoScreenState extends State<ApiDemoScreen> {
         title: const Text('API Demo Screen'),
         backgroundColor: Colors.green,
       ),
-      body: Placeholder(),
+      body: ListView(
+        children: [
+          ElevatedButton(onPressed: () {
+            getRandomQuote();
+          }, child: const Text("GET QUOTE",
+
+          )),
+          Text(quote,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30)),
+        ],
+      ),
     );
   }
 }
